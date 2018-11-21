c8e658e7dcf6a7b24c119400f12ec1dc94697c22
raj <rajvarma007009@gmail.com>
2018-11-21 12:03:41 +0530

    Update Program.cs

--- SaveTheData/Program.cs
+++ SaveTheData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace SaveTheData
     {
         static void Main(string[] args)
         {
+            string filepath = @"D:\test.txt";
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                writer.WriteLine("some text");
+                writer.WriteLine("HAPPY HOURS");
+            }
+            string result = Path.GetFileNameWithoutExtension( @"D:\test.txt");
+            Console.WriteLine(result);
+            string file = File.ReadAllText(filepath);
+            Console.WriteLine(file);
+            Console.ReadKey();
         }
     }
 }
